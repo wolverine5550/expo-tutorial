9c8abe78b24140295cb85b5f71423b28dc9d6893
wolverine5550 <apascar@gmail.com>
2024-10-29 20:24:42 -0400

    Adding Navigation

---
+++ app/(tabs)/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+// Note: To run the app, may need to run the command "watchman".
+// Also may have to run "open -a" Simulator to open the iPhone simulator before "npm start
+
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>Home Screen</Text>
+        <Link href="/about" style={styles.button}>
+        Go to About screen
+        </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
