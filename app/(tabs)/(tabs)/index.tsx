0d32cedf180df00f8feedf31943eb6f4f0d42fc2
wolverine5550 <apascar@gmail.com>
2024-10-30 10:03:02 -0400

    Adding the image picker

--- app/(tabs)/(tabs)/index.tsx
+++ app/(tabs)/(tabs)/index.tsx
@@ -2,6 +2,7 @@
 // Also may have to run "open -a" Simulator to open the iPhone simulator before "npm start
 
 import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from '@/components/Button';
 import ImageViewer from '@/components/ImageViewer';
@@ -9,13 +10,27 @@ import ImageViewer from '@/components/ImageViewer';
 const PlaceholderImage = require('@/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo" onPress={/* @tutinfo Add this. */pickImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
